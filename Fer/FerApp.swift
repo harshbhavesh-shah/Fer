31236a679b48719623790ea5f8d5435ba270b3ed
Harsh Shah <harsh@Harshs-MacBook-Air.local>
2026-07-29 15:42:07 +0530

    emtpy project

---
+++ Fer/FerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FerApp.swift
+//  Fer
+//
+//  Created by Harsh Shah on 29/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
